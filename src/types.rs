2b5e14321e4b6cb305d92b5cea83554ec8f04eed
nooberfsh <nooberfsh@gmail.com>
2020-05-05 16:22:38 +0800

    remove parse method in trait `Presto`

--- src/types.rs
+++ src/types.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::hash::Hash;
 
 use serde::ser::{self, Serialize, SerializeSeq, SerializeStruct, Serializer};
-use serde_json::Value;
 
 use crate::models;
 
@@ -11,24 +10,8 @@ pub struct Error;
 pub trait Presto {
     type ValueType<'a>: Serialize;
 
+    fn value(&self) -> Self::ValueType<'_>;
     fn ty() -> PrestoTy;
-    fn parse(ty: &PrestoTy, v: Value) -> Result<Self, Error>
-    where
-        Self: Sized;
-    fn deserialize_as_map_key(ty: &PrestoTy, s: &str) -> Result<Self, Error>
-    where
-        Self: Sized,
-    {
-        Err(Error)
-    }
-    fn serialize_as_map_key(&self) -> Result<String, Error>
-    where
-        Self: Sized,
-    {
-        Err(Error)
-    }
-
-    fn value_type(&self) -> Self::ValueType<'_>;
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -67,89 +50,49 @@ impl PrestoTy {
 impl Presto for i32 {
     type ValueType<'a> = &'a i32;
 
-    fn value_type(&self) -> Self::ValueType<'_> {
+    fn value(&self) -> Self::ValueType<'_> {
         self
     }
 
     fn ty() -> PrestoTy {
         PrestoTy::Integer
     }
-    fn parse(ty: &PrestoTy, v: Value) -> Result<Self, Error> {
-        match (ty, v) {
-            (PrestoTy::Integer, Value::Number(n)) if n.is_i64() => Ok(n.as_i64().unwrap() as i32),
-            _ => Err(Error),
-        }
-    }
 }
 
 impl Presto for String {
     type ValueType<'a> = &'a String;
 
-    fn value_type(&self) -> Self::ValueType<'_> {
+    fn value(&self) -> Self::ValueType<'_> {
         self
     }
     fn ty() -> PrestoTy {
         PrestoTy::Varchar
-    }
-    fn parse(ty: &PrestoTy, v: Value) -> Result<Self, Error> {
-        match (ty, v) {
-            (PrestoTy::Varchar, Value::String(s)) => Ok(s),
-            _ => Err(Error),
-        }
     }
 }
 
 impl<T: Presto> Presto for Vec<T> {
     type ValueType<'a> = Vec<T::ValueType<'a>>;
 
-    fn value_type(&self) -> Self::ValueType<'_> {
-        self.iter().map(|t| t.value_type()).collect()
+    fn value(&self) -> Self::ValueType<'_> {
+        self.iter().map(|t| t.value()).collect()
     }
 
     fn ty() -> PrestoTy {
         PrestoTy::Array(Box::new(T::ty()))
-    }
-    fn parse(ty: &PrestoTy, v: Value) -> Result<Self, Error> {
-        if let PrestoTy::Array(ty) = ty {
-            if let Value::Array(values) = v {
-                let mut ret = Vec::with_capacity(values.len());
-                for v in values {
-                    let t = T::parse(&ty, v)?;
-                    ret.push(t);
-                }
-                return Ok(ret);
-            }
-        }
-        Err(Error)
     }
 }
 
 impl<K: Presto + Eq + Hash, V: Presto> Presto for HashMap<K, V> {
     type ValueType<'a> = Vec<(K::ValueType<'a>, V::ValueType<'a>)>;
 
-    fn value_type(&self) -> Self::ValueType<'_> {
+    fn value(&self) -> Self::ValueType<'_> {
         self.iter()
-            .map(|(k, v)| (k.value_type(), v.value_type()))
+            .map(|(k, v)| (k.value(), v.value()))
             .collect()
     }
 
     fn ty() -> PrestoTy {
         PrestoTy::Map(Box::new(K::ty()), Box::new(V::ty()))
-    }
-
-    fn parse(ty: &PrestoTy, v: Value) -> Result<Self, Error> {
-        if let PrestoTy::Map(t1, t2) = ty {
-            if let Value::Object(o) = v {
-                let mut ret = HashMap::new();
-                for (k, v) in o {
-                    let k = K::deserialize_as_map_key(&t1, &k)?;
-                    let v = V::parse(&t2, v)?;
-                    ret.insert(k, v);
-                }
-                return Ok(ret);
-            }
-        }
-        Err(Error)
     }
 }
 
