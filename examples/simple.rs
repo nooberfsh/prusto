4aeed06555da32b171c89eeef2c5a87af7e60363
nooberfsh <nooberfsh@gmail.com>
2020-05-21 22:48:48 +0800

    add simple example

---
+++ examples/simple.rs
@@ -0,0 +1,30 @@
+#![allow(incomplete_features)]
+#![feature(generic_associated_types)]
+
+use std::env::var;
+
+use dotenv::dotenv;
+use prusto::{ClientBuilder, Row};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let user = var("USER").unwrap();
+    let host = var("HOST").unwrap();
+    let port = var("PORT").unwrap().parse().unwrap();
+    let catalog = var("CATALOG").unwrap();
+    let sql = var("SQL").unwrap();
+
+    let cli = ClientBuilder::new(user, host)
+        .port(port)
+        .catalog(catalog)
+        .build()
+        .unwrap();
+
+    let data = cli.get_all::<Row>(sql).await.unwrap().into_vec();
+
+    for r in data {
+        println!("{:?}", r)
+    }
+}
